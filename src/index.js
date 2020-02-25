568864db11cf3525b7be356da73febb2eaf39e4d
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-25 19:52:42 +0000

    Redux working as expected in example in index.js.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import {addExpense} from "./actions/expenses";
+import {setTextFilter} from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "./styles/styles.scss"
 
-ReactDOM.render(<AppRouter/>, document.getElementById('root'));
+const store = configureStore();
+
+store.dispatch(addExpense({
+    description: "Water Bill",
+}));
+
+store.dispatch(addExpense({
+    description: "Gas BIll",
+}));
+
+store.dispatch(setTextFilter("water"));
+
+console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
+
+ReactDOM.render(<AppRouter store={store}/>, document.getElementById('root'));
