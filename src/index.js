7a04085f280e16a63908ba81a55a0e00d7927d2c
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-25 12:03:49 +0000

    Set up react router to render various routes to the browser.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
-ReactDOM.render(<div>hello</div>, document.getElementById('root'));
+
+const ExpenseDashboardPage = () => (
+    <>
+        This is my dashboard component
+    </>
+);
+
+const AddExpensePage = () => (
+    <>
+        This is my add expense page
+    </>
+);
+
+const EditExpensePage = () => (
+    <>
+        This is my edit expense page
+    </>
+);
+
+const HelpPage = () => (
+    <>
+        This is my help page
+    </>
+);
+
+const routes = (
+    <Router>
+        <>
+            <Route path={"/"} component={ExpenseDashboardPage} exact={true}/>
+            <Route path={"/create"} component={AddExpensePage}/>
+            <Route path={"/edit"} component={EditExpensePage}/>
+            <Route path={"/help"} component={HelpPage}/>
+        </>
+    </Router>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
 
