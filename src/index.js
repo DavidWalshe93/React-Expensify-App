9d6a4024040e2b41777d55d2ac0241f40f1696c5
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-25 15:09:50 +0000

    Added some slight styling for NavLink when active. Added Navbar for page navigation.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Link, NavLink, Route, Switch} from "react-router-dom";
+import "./styles/styles.scss"
 
 
 const ExpenseDashboardPage = () => (
@@ -27,13 +28,34 @@ const HelpPage = () => (
     </>
 );
 
+const NotFoundPage = () => (
+    <>
+        Page not found. <Link to={"/"}>Go to Home</Link>
+    </>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to={"/"} activeClassName={"is-active"} exact={true}>Home</NavLink>
+        <NavLink to={"/create"} activeClassName={"is-active"}>Create Expense</NavLink>
+        <NavLink to={"/edit"} activeClassName={"is-active"}>Edit Expense</NavLink>
+        <NavLink to={"/help"} activeClassName={"is-active"}>Help</NavLink>
+    </header>
+);
+
 const routes = (
     <Router>
         <>
-            <Route path={"/"} component={ExpenseDashboardPage} exact={true}/>
-            <Route path={"/create"} component={AddExpensePage}/>
-            <Route path={"/edit"} component={EditExpensePage}/>
-            <Route path={"/help"} component={HelpPage}/>
+            <Header/>
+            {/*Switch attempts to match any route in order*/}
+            <Switch>
+                <Route path={"/"} component={ExpenseDashboardPage} exact={true}/>
+                <Route path={"/create"} component={AddExpensePage}/>
+                <Route path={"/edit"} component={EditExpensePage}/>
+                <Route path={"/help"} component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
         </>
     </Router>
 );
